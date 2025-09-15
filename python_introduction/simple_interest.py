52e1a1ab562512b76699013b11b1cc85453f3904
Gabriel Owusu <owusugabriel803@gmail.com>
2025-09-15 20:41:02 +0000

    Add files via upload

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000    
+rate = 0.05         
+time = 3
+interest = principal * rate * time
+print(f"Principal Amount: ${principal}")
+print(f"Annual Interest Rate: {rate * 100}%")
+print(f"Time Period: {time} years")
+print(f"Simple Interest Earned: ${interest}")
